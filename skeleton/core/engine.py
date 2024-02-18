628685458c86e0bd9b30abd50267cd9d62d70de6
Dani_Dimitrov <mahckadani@gmail.com>
2024-02-18 16:22:55 +0200

    engine saves output to file. Every time the program runs the ouput file is cleared and stores the new output.

--- skeleton/core/engine.py
+++ skeleton/core/engine.py
@@ -2,8 +2,9 @@ from skeleton.core.command_factory import CommandFactory
 
 
 class Engine:
-    def __init__(self, factory: CommandFactory):
+    def __init__(self, factory: CommandFactory, output_file="output.txt"):
         self._command_factory = factory
+        self.output_file = output_file
 
     def start(self):
         output: list[str] = []
@@ -15,5 +16,8 @@ class Engine:
             command = self._command_factory.create(input_line)
 
             output.append(command.execute())
+
+        with open(self.output_file, 'w') as file:
+            file.write('\n'.join(output))
 
         print('\n'.join(output))
